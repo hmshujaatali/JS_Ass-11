ff1de4eb85955882af449f4740cab46fe83b56fd
hmshujaatali <81732578+hmshujaatali@users.noreply.github.com>
2021-08-18 15:16:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// // function abc(xyz){
+   
+// //    document.write(xyz)
+// // }
+// // function hoo(xyz){
+// //    var xyz="hayeeeee hoooo"
+// //    document.write(xyz)
+// // }
+
+// var a=0
+// function increase(){
+// a++
+// document.getElementById("a").innerHTML=a
+// }
+// function decrease(){
+// a--
+// document.getElementById("a").innerHTML=a
+// }
---
+++ appp.js
@@ -0,0 +1,40 @@
+// function deleteRow(r) {
+//     var i = r.parentNode.parentNode.rowIndex;
+//     document.getElementById("myTable").deleteRow(i);
+//   }
+// var a=1
+// function increase(){
+// a=a+1
+// }
+
+// function increase(){
+// a=a-1
+// }
+
+// function click() {
+//     clicks += 1;
+//     document.getElementById("clicks").innerHTML = clicks;
+// };
+
+
+// var clicks = 0;
+
+
+// function increase() {
+//     clicks += 1;
+//     document.getElementById("clicks").innerHTML = clicks;
+// };
+// function decrease() {
+//     clicks -= 1;
+//     document.getElementById("clicks").innerHTML = clicks;
+// };
+
+// var a=0
+// function increase(){
+//     a++
+//     document.getElementById("click").innerHTML=a
+// }
+// function decrease(){
+//     a--
+//     document.getElementById("click").innerHTML=a
+// }
